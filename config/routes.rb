b6aebc2eca386f92e826cad6e832de293c982ce8
Josh Del Sesto <Chefdel70@gmail.com>
2019-03-29 14:51:56 -0400

    refactored routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,5 @@ Rails.application.routes.draw do
   get '/about',  to: 'static_pages#about'
   get '/help',  to: 'static_pages#help'
   get '/home',  to: 'static_pages#home'
-  # get 'static_pages/home'
-  # get 'static_pages/help'
-  # get 'static_pages/about'
   get 'sessions/new'
-  # root 'static_pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
